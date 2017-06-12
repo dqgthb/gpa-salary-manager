488d4f4254da3dad015e9929bbf8423bc5011f28
DQmacOS <deonquebec@gmail.com>
2017-06-12 13:32:51 +0900

    ; cut

--- worker.h
+++ worker.h
@@ -8,13 +8,14 @@ protected:
     int salary;
 
 public:
-    worker(){salary=0;};
+    worker(){salary=0;}
     worker(int _salary){salary=_salary;}
     worker(worker& origin){
         salary=origin.salary;
-        std::cout << "copying..." << "\n";}
-    int getSalary(){return salary;};
-    void setSalary(int _salary){salary=_salary;};
+        std::cout << "copying..." << "\n";
+    }
+    int getSalary(){return salary;}
+    void setSalary(int _salary){salary=_salary;}
     void printinfo(){
         std::cout << std::setw(20) << std::left << "<< salary >> " << std::setw(20) << " goes to memory: " << std::setw(20) << &salary << std::setw(25) << " and set(get) value of " << salary << "\n";
     }
